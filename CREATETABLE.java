013d9b2c1d3a8375a33a64b97c41ead991a103d2
kambamshreya <90392402+kambamshreya@users.noreply.github.com>
2022-06-26 16:47:42 +0530

    Add files via upload

---
+++ CREATETABLE.java
@@ -0,0 +1,20 @@
+import java.sql.*; 
+public class CREATETABLE
+{ 
+public static void main(String args[]) 
+{
+try 
+{ 
+Class.forName("com.mysql.jdbc.Driver"); 
+Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Moviedetails","root",""); 
+Statement st=con.createStatement(); 
+st.executeUpdate("create table Movies ( Name varchar(25), Actor varchar(20), Actress varchar(25), Director varchar(20), Year int(20))"); 
+System.out.println("Table Created"); 
+con.close(); 
+} 
+catch(Exception e) 
+{ 
+System.out.println(e); 
+} 
+} 
+} 
